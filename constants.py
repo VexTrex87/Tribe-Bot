f6c4a7e605c4525cdfa6739f3d0dd9398010d8d6
Vex87 <AndrewKyleGuilas@gmail.com>
2021-05-03 17:57:22 -0400

    reformatted settings

--- constants.py
+++ constants.py
@@ -10,14 +10,18 @@ EXTENSIONS = [
 DEFAULT_GUILD_DATA = {
     "guild_id": None,
     "prefix": "?",
+
     "daily_reward": 10,
     "point_channels": [],
     "points_per_message": 1,
     "message_cooldown": 10,
+
     "qotd_channel": None,
     "aotd_keywords": ["qotd", "aotd"],
     "points_per_aotd": 50,
-    "giveaway_channel": None,
+
+    "giveaway_channel": None
+    ,
     "roblox_groups": [],
     "group_award": 500,
     "roblox_games": [],
