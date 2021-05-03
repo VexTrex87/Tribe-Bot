1099de6f9984ccca5a4a52cd3562dea9ad87be86
Vex87 <AndrewKyleGuilas@gmail.com>
2021-05-03 13:18:37 -0400

    added daily reward setting

--- cogs/default.py
+++ cogs/default.py
@@ -302,6 +302,38 @@ class default(commands.Cog, description = "Default commands and commands for set
                     }, guild_data))
                     await asyncio.sleep(WAIT_DELAY)
                     continue
+                elif name == "daily_reward":
+                    try:
+                        value = int(value)
+                    except ValueError:
+                        await response.edit(embed = create_embed({
+                            "title": f"The daily reward ({value}) must be a number",
+                            "color": discord.Color.red(),
+                            "inline": True,
+                        }, guild_data))
+                        await asyncio.sleep(WAIT_DELAY)
+                        continue
+
+                    if value < 0:
+                        await response.edit(embed = create_embed({
+                            "title": f"The daily reward ({value}) must be greater than or equal to 0",
+                            "color": discord.Color.red(),
+                            "inline": True,
+                        }, guild_data))
+                        await asyncio.sleep(WAIT_DELAY)
+                        continue
+
+                    new_guild_data = get_guild_data(context.guild.id)
+                    new_guild_data["daily_reward"] = value
+                    save_guild_data(new_guild_data)
+
+                    await response.edit(embed = create_embed({
+                        "title": f"Changed daily reward to {value}",
+                        "inline": True,
+                        "color": discord.Color.green()
+                    }, guild_data))
+                    await asyncio.sleep(WAIT_DELAY)
+                    continue
                 else:
                     await response.edit(embed = create_embed({
                         "title": f"{name} is an invalid setting",
